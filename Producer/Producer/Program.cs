27ee0856344486481a1823beff7308f2585a6015
Guilherme Paschoal <paschoal.gui@gmail.com>
2022-05-13 21:33:32 -0300

    add files

---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("messageQueue", false, false, false, null);
+
+    var objToSerialize = new { MessageToBeSerialized = "This message will be serialized", version = "1.0" };
+
+    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(objToSerialize));
+
+    channel.BasicPublish("", "messageQueue", null, body:body);
+
+    Console.WriteLine(" [x] Sent {0}", objToSerialize);
+}
+
+// Console.WriteLine(" Press [enter] to exit.");
+// Console.ReadLine();
